6b4687e1dfd926b747ffcbb7399f9085b61b45c2
溏心萌木CutePlank <34810662+CutePlank233@users.noreply.github.com>
2024-12-15 22:43:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@
 // @description  Transform your entire browsing experience into an adorable, charming world of green! Green is not just a color; it’s the signature hue of railways, especially the beloved regional passenger trains that sport a cute, vibrant green. This script will infuse your browser with that same delightful shade, making everything feel as cheerful and inviting as those trains. Imagine every webpage wrapped in this playful green, bringing a sense of nostalgia and warmth, reminiscent of train journeys through beautiful landscapes. Let’s make your online experience as lovable and charming as those cute railway cars, spreading joy and color with every click!
 // @author       Makuragi Wood
 // @grant        none
+// @downloadURL  https://raw.githubusercontent.com/CutePlank233/GreenifyWebsite/refs/heads/main/script.js
+// @updateURL    https://raw.githubusercontent.com/CutePlank233/GreenifyWebsite/refs/heads/main/script.js
 // ==/UserScript==
 
 (function() {
